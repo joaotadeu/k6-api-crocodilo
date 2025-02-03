9e952de16cd7f32b7bb0cb2e30924f43fc6b1816
João Tadeu Soares Pereira <ojoaotadeu@gmail.com>
2025-02-03 00:28:22 -0300

    adicionando mais testes de carga

---
+++ k6_tests/tests/scenarios/POST/stress_test_register.js
@@ -0,0 +1,47 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+import { SharedArray } from 'k6/data';
+
+const dadosCrocodilos = new SharedArray("dadosCrocodilos", function () {
+    const filePath = '/Users/joaotadeu/Documents/Workspace/JavaScript/k6/k6.io/k6_tests/tests/data/dados.json';
+    let fileContents = open(filePath);
+    return JSON.parse(fileContents).crocodilos;
+});
+
+export const options = {
+    stages: [
+        { duration: '5s', target: 5 },
+        { duration: '5s', target: 5 },
+        { duration: '2s', target: 50 },
+        { duration: '2s', target: 50 },
+        { duration: '5s', target: 1 },
+        { duration: '5s', target: 0 },
+    ],
+    thresholds: {
+        checks: ['rate > 0.90'],
+        http_req_failed: ['rate < 0.01'],
+    }
+};
+
+export default function () {
+    const BASE_URL = 'https://test-api.k6.io';
+
+    const user = `${Math.random()}@gmail.com`;
+    const pass = 'user123';
+
+    console.log( user + pass)
+
+    const response = http.post(`${BASE_URL}/user/register/`, {
+        username: user,
+        first_name: 'croco',
+        last_name: 'dino',
+        email: user,
+        password: pass
+    });
+
+    check(response, {
+        'sucesso ao registrar': (r) => r.status === 201
+    });
+
+    sleep(1)
+}
